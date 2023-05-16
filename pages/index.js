dbc9b2a5c35e281af83a8639f09535fa0a13bb9f
Cody Silvers <codysilvers1993@hotmail.com>
2023-05-15 20:22:33 -0400

    still working on some issues

--- pages/index.js
+++ pages/index.js
@@ -6,64 +6,84 @@ import logo from '../public/bmex-ico.png'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Bayesian Mass Explorer</title>
-        <meta name="description" content="Bayesian nuclear mass explorer" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className={styles.container}>
+        <Head>
+          <title>Bayesian Mass Explorer</title>
+          <meta name="description" content="Bayesian nuclear mass explorer" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <p style={{ width: '60%', margin: 'auto' }}>
+        <main className={styles.main} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
           <a href="https://bmex.dev">
-            <Image alt="BMEX Logo" src={logo} style={{ marginTop: '485px' }} />
+            <Image alt="BMEX Logo" src={logo} />
           </a>
-        </p>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://bmex.dev">BMEX!</a>
-        </h1>
+          <h1 className={styles.title}>
+            Welcome to BMEX!
+          </h1>
+          <p className={styles.description}>
+            Explore masses in the Bayesian way!
+          </p>
 
-        <p className={styles.description}>
-          Explore masses in the Bayesian way!  
-        </p>
 
-        <div className={styles.grid}>
-	  <Link href="/data">
-          <a className={styles.card}>
-            <h2>Masses and Filters &rarr;</h2>
-            <p>Binding energies and related quantities for experiment and various models.</p>
-          </a>
-          </Link>
-	  <Link href="/emulator">
-          <a className={styles.card}>
-            <h2>Emulator &rarr;</h2>
-            <p>Binding energies and charge radii using a reduced basis method emulator.</p>
-          </a>
-          </Link>
-          <Link href="/gpe" className={styles.card}>
-	  <a className={styles.card}>
-            <h2>Gaussian Process Extrapolator &rarr;</h2>
-            <p>Simple Gaussian Process explorer for nuclear models.</p>
-          </a>
-	  </Link>
-          <Link href="/pesnet" className={styles.card}>
-	  <a className={styles.card}>
-            <h2>PESnet &rarr;</h2>
-            <p>Dynamically produce nuclear potential energy surfaces using machine learning.</p>
-          </a>
-	  </Link>
-	  <Link href="https://beta.bmex.dev/masses">
-          <a className={styles.card}>
-            <h2>Masses and Filters Beta &rarr;</h2>
-            <p>Binding energies and related quantities for experiment and various models, including additonal beta features.</p>
-          </a>
-          </Link>
-        </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a href="https://github.com/massexplorer/bmex-static">&copy; 2022 BMEX Team</a>
-      </footer>
-    </div>
+
+
+          <div className={styles.grid}>
+            <div className={styles.column}>
+              <Link href="/data" passHref>
+                <a className={styles.card}>
+                  <h2>Masses and Filters &rarr;</h2>
+                  <p>Binding energies and related quantities for experiment and various models.</p>
+                </a>
+              </Link>
+
+              <Link href="/emulator" passHref>
+                <a className={styles.card}>
+                  <h2>Emulator &rarr;</h2>
+                  <p>Binding energies and charge radii using a reduced basis method emulator.</p>
+                </a>
+              </Link>
+            </div>
+
+            <div className={styles.column}>
+              <Link href="/gpe" passHref>
+                <a className={styles.card}>
+                  <h2>Gaussian Process Extrapolar &rarr;</h2>
+                  <p>Simple Gaussian Process explorer for nuclear models.</p>
+                </a>
+              </Link>
+
+              <Link href="/pesnet" passHref>
+                <a className={styles.card}>
+                  <h2>PESnet &rarr;</h2>
+                  <p>Dynamically produce nuclear potential energy surfaces using machine learning.</p>
+                </a>
+              </Link>
+            </div>
+          </div>
+
+
+          <div className={styles.grid}>
+            <div className={styles.columnFull}>
+              <Link href="https://beta.bmex.dev/masses" passHref>
+                <a className={styles.card}>
+                  <h2>Masses and Filters Beta &rarr;</h2>
+                  <p>Binding energies and related quantities for experiment and various models, including additional beta features.</p>
+                </a>
+              </Link>
+            </div>
+          </div>
+
+
+
+
+
+
+        </main>
+
+        <footer className={styles.footer}>
+          <a href="https://github.com/massexplorer/bmex-static">&copy; 2022 BMEX Team</a>
+        </footer>
+      </div>
   )
 }
